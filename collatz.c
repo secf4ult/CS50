a2869fb2b455b09a30c64f1545fe2b9f9df9d36e
secf4ult <secf4ult@gmail.com>
2018-01-23 13:28:31 +0000

    add residual file

---
+++ collatz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int collatz(int n);
+
+int main(void)
+{
+    printf("%d", collatz(3));
+
+    return 0;
+}
+
+int collatz(int n)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    else if (n % 2 == 0)
+    {
+        return 1 + collatz(n / 2);
+    }
+    else
+    {
+        return 1 + collatz(3 * n + 1);
+    }
+}
